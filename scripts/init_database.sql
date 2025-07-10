9e3d71cbf0bb8a723e7c342cec9ab9f0a300873a
Rutuja Navale <139907839+Rutuja-117@users.noreply.github.com>
2025-07-10 18:13:04 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,6 @@
+CREATE DATABASE dw_project;  -- optional: can be used for overall management/metadata
+CREATE DATABASE Bronze;      -- raw/staging layer
+CREATE DATABASE Silver;      -- cleaned/validated layer
+CREATE DATABASE Gold;        -- curated analytics layer
+
 
